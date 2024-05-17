3affdb8404abf8f3dae9e99d86f5922d608d5c83
psyofrelief <phariedd@gmail.com>
2024-05-18 01:01:59 +0800

    Upd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,16 +9,17 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Honk&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
-
+    <link href="{{ mix('public/build/assets/app-CBTPaFpW.css') }}" rel="stylesheet">
+    <script src="{{ mix('public/build/assets/app-DkDdL2UM.js') }}" defer></script>
 
 </head>
 
 <body class="min-h-screen flex flex-col text-center font-spaceMono">
 
-    <img id="bg-img"alt="cloud bg" src="{{ asset('/img/cloud-bg.svg') }}" class="fixed z-[-1] bottom-0 left-0 right-0 w-full" />
+    <img id="bg-img" alt="cloud bg" src="{{ asset('/img/cloud-bg.svg') }}" class="fixed z-[-1] bottom-0 left-0 right-0 w-full" />
     <x-navbar />
-    
-        @yield('content')
+
+    @yield('content')
     <x-footer />
     @yield('scripts')
 </body>
--- webpack.mix.mjs
+++ webpack.mix.mjs
@@ -2,6 +2,9 @@ const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 
 mix.js('resources/js/app.js', 'public/js')
+.css('resources/css/app.css', 'public/css', [
+      tailwindcss('tailwind.config.js'),
+])
    .postCss('resources/css/app.css', 'public/css', [
       tailwindcss('tailwind.config.js'),
    ]);
