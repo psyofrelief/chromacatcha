3a667699fead8b927fa14acda3ed966de6995d7e
psyofrelief <phariedd@gmail.com>
2024-05-18 01:27:00 +0800

    Add css files to public

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,12 +9,13 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Honk&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+    <link href="{{ mix('public/css/app.css') }}" rel="stylesheet">
+     
 
 </head>
 
 <body class="min-h-screen flex flex-col text-center font-spaceMono">
 
-    <img id="bg-img" alt="cloud bg" src="{{ asset('/img/cloud-bg.svg') }}" class="fixed z-[-1] bottom-0 left-0 right-0 w-full" />
     <x-navbar />
 
     @yield('content')
