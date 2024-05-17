cbf6c8b072024635e2812bff7f0e45d290d8f3df
psyofrelief <phariedd@gmail.com>
2024-05-18 01:18:21 +0800

    Remove all css content and fonts

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,3 +1,3 @@
 <nav class="bg-secondary flex justify-center ">
-  <a class="btn btn-ghost text-3xl font-honk sm:text-4xl" href="/">ChromaCatcha</a>
+  <a class="btn btn-ghost text-3xl  sm:text-4xl" href="/">ChromaCatcha</a>
 </nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,16 +5,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Honk&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
     <link href="{{ mix('public/build/assets/app-CBTPaFpW.css') }}" rel="stylesheet">
     <script src="{{ mix('public/build/assets/app-DkDdL2UM.js') }}" defer></script>
 
 </head>
 
-<body class="min-h-screen flex flex-col text-center font-spaceMono">
+<body class="min-h-screen flex flex-col text-center ">
 
     <img id="bg-img" alt="cloud bg" src="{{ asset('/img/cloud-bg.svg') }}" class="fixed z-[-1] bottom-0 left-0 right-0 w-full" />
     <x-navbar />
