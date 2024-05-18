38f1f8e4c349e49b9301e6c9fb6b2c3c0dd4bf83
psyofrelief <phariedd@gmail.com>
2024-05-18 13:41:42 +0800

    Upd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html data-theme="cyberpunk" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="utf-8">
@@ -10,7 +10,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Honk&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
     <link href="{{ asset('/build/assets/app-DPsmAb3R.css') }}" rel="stylesheet">
-    <script src="{{ asset('/build/assets/app-DkDdL2UM.js') }}" defer></script>
+    <script src="{{ asset('/build/assets/app-BuF5rfZ9.js') }}" defer></script>
 </head>
 
 <body class="min-h-screen flex flex-col relative text-center font-spaceMono">
