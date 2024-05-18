2cef0cd66d0d948567a8ba22a506ef85008b973b
psyofrelief <phariedd@gmail.com>
2024-05-18 14:16:13 +0800

    Upd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Honk&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
-    <link href="{{ asset('/build/assets/app-DPsmAb3R.css') }}" rel="stylesheet">
-    <script src="{{ asset('/build/assets/app-BuF5rfZ9.js') }}" ></script>
+    <link href="{{ asset('build/assets/app-DPsmAb3R.css') }}" rel="stylesheet">
+    <script src="{{ asset('build/assets/app-BuF5rfZ9.js') }}" ></script>
 </head>
 
 <body class="min-h-screen flex flex-col relative text-center font-spaceMono">
